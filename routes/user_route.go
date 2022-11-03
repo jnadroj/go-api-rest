b9db4931c1b69179047025cc6c3a8bb634c3782e
jnadroj <vednadroj@gmail.com>
2022-11-02 22:39:09 -0500

    feat: update and delete endpoint and testing of all endpoints with extension rest client of vscode

--- routes/user_route.go
+++ routes/user_route.go
@@ -2,11 +2,14 @@ package routes
 
 import (
 	"apirest/controllers"
+
 	"github.com/labstack/echo/v4"
 )
 
 func UserRoute(e *echo.Echo) {
-	e.POST("/user", controllers.CreateUser)
-	e.GET("/user/:userId", controllers.GetAUser) 
 	e.GET("/users", controllers.GetAllUsers) 
+	e.GET("/user/:userId", controllers.GetAUser) 
+	e.POST("/user", controllers.CreateUser)
+	e.PATCH("/user/:userId", controllers.EditAUser)
+	e.DELETE("/user/:userId", controllers.DeleteAUser)
 }
